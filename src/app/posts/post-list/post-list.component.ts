1373e2d0666e5b108f87d7f5dfd02fc3de2e2eaf
Nico <nicolas.rouge777@gmail.com>
2019-07-30 00:23:54 +0200

    post from firebase database ok

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Post } from '../post';
+import { PostService } from '../post.service';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-
-  constructor() { }
+  
+  posts: Observable<Post[]>
+  
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
+    this.posts = this.postService.getPosts()
+    console.log(this)
+    console.log("HELLO")
   }
 
 }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore'
+
+import { Post } from './post'
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  postsCollection: AngularFirestoreCollection<Post>
+  postDoc: AngularFirestoreDocument<Post>
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+    this.postsCollection = this.afs.collection('posts', ref =>
+      ref.orderBy('published', 'desc')
+    )
+  }
+
+  //oh my god le pipe fallait le trouver :/
+  getPosts() {
+    return this.postsCollection.snapshotChanges().pipe(map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Post
+        const id = a.payload.doc.id
+        return { id, ...data }
+      })
+    }));
+  }
+
 }
