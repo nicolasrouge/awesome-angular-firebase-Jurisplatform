0494740defc9a840e3884d969e3e64268a9b3bd1
Nico <nicolas.rouge777@gmail.com>
2019-07-31 00:33:18 +0200

    image display at home page

--- src/app/posts/post-dashboard/post-dashboard.component.ts
+++ src/app/posts/post-dashboard/post-dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/auth.service';
 import { PostService } from '../post.service';
+import { Observable } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/storage'
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-dashboard',
@@ -13,15 +16,19 @@ export class PostDashboardComponent implements OnInit {
   image: string = null
   content: string
 
-  buttonText: string= "Create Post"
+  buttonText: string = "Create Post"
+  uploadPercent: Observable<number>
+  downloadURL: Observable<string>
 
-
-  constructor(private auth: AuthService, private postService: PostService) { }
+  constructor(
+    private auth: AuthService, 
+    private postService: PostService, 
+    private storage: AngularFireStorage) { }
 
   ngOnInit() {
   }
 
-  createPost(){
+  createPost() {
     const data = {
       author: this.auth.authState.displayname || this.auth.authState.email,
       authorId: this.auth.currentUserId,
@@ -31,9 +38,31 @@ export class PostDashboardComponent implements OnInit {
       title: this.title
     }
     this.postService.create(data)
-    this.title=''
-    this.content= ''
+    this.title = ''
+    this.content = ''
     this.buttonText = 'Post Created!'
     setTimeout(() => (this.buttonText = "Create Post"), 3000)
   }
+
+  uploadImage(event) { 
+    const file = event.target.files[0]
+    const path = `posts/${file.name}`
+    console.log(event.target.files)
+    if(file.type.split('/')[0] !== 'image'){
+      return alert('only image files')
+    }
+    else {
+      const task = this.storage.upload(path, file);
+      const ref = this.storage.ref(path);
+      this.uploadPercent = task.percentageChanges();
+      console.log('Image uploaded!');
+      task.snapshotChanges().pipe(
+        finalize(() => {
+          this.downloadURL = ref.getDownloadURL()
+          this.downloadURL.subscribe(url => (this.image = url));
+        })
+      )
+      .subscribe();
+    }
+   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,21 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from '../post';
 import { PostService } from '../post.service';
+import { AuthService } from 'src/app/core/auth.service';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  
+
   posts: Observable<Post[]>
-  
-  constructor(private postService: PostService) { }
+
+  constructor(private postService: PostService, public auth: AuthService) { }
 
   ngOnInit() {
     this.posts = this.postService.getPosts()
     console.log(this)
     console.log("HELLO")
   }
-
+  delete(id: string) {
+    console.log("DELETE method")
+    this.postService.delete(id)
+  }
 }
