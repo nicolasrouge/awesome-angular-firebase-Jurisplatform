1cb59075d8077200ede3e529bf74e8ce0eaf3638
Nico <nicolas.rouge777@gmail.com>
2019-08-04 18:00:44 +0200

    update home page

--- src/app/page/homepage/homepage.component.ts
+++ src/app/page/homepage/homepage.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
 
   page = {
-    title: 'Home',
-    subtitle: 'Welcome Home!',
-    content: 'Some home content.',
-    image: 'assets/images/blueberry.jpg'
+    title: 'Bonjour et bienvenue',
+    subtitle: 'ici nous sommes sur la page d\'acueil ! Vous pouvez consulter les articles via l\'onglet \'Blog\'',
+    content: 'Nous couvrons les secteurs juridique des stratups et nouvelles technologies. Vous pourrez comprendre les enjeux techniques et juridiques des nouvelles technologies',
+    image: 'assets/images/logodesign.png'
   };
 
   constructor() { }
