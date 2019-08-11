84bb3379438af915f7c0bd17b91c9032ede85204
Nico <nicolas.rouge777@gmail.com>
2019-08-11 16:08:11 +0200

    fix bug home page

--- src/app/page/header/header.component.ts
+++ src/app/page/header/header.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  page = {
+    title: 'Bonjour et bienvenue',
+    subtitle: 'ici nous sommes sur la page d\'acueil ! Vous pouvez consulter les articles via l\'onglet \'Blog\'',
+    content: 'Nous couvrons les secteurs juridique des stratups et nouvelles technologies. Vous pourrez comprendre les enjeux techniques et juridiques des nouvelles technologies',
+    image: 'assets/images/logodesign.png'
+  };
+
   constructor() { }
 
   ngOnInit() {
