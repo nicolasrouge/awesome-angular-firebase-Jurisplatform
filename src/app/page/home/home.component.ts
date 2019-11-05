6902adbf463862a1fb9eaabe4ae87326afd7ee91
Nico <nicolas.rouge777@gmail.com>
2019-11-05 15:36:14 +0100

    add dropdown list menu

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,7 +11,12 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
+    (function ($) {
+      $(document).ready(function(){
+        console.log("Hello from jQuery!");
+      });
+    })(jQuery);
   }
 
 }
